c18414bd0fb05eec5258e987651c7540f513e5cb
ryanshim24 <ryanshim24@yahoo.com>
2014-11-17 21:53:33 -0800

    added color to the nav bar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,23 @@ $(document).ready(function(){
   $(".reset-link").on("click", function() {
     $('#logModal').modal('hide');
   });
+
+  var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background', 'rgba(255,255,255,0.9)');
+          $('.navbar-default').css('border-bottom-color', 'rgba(0, 0, 0, 0.0980392)');
+
+       } else {
+          $('.navbar-default').css('background-color', 'transparent');
+          $('.navbar-default').css('border-bottom-color', 'rgba(0, 0, 0, 0)');
+          $(".navbar-default").css('background', 'none ');
+       }
+   });
+    }
+
 });
