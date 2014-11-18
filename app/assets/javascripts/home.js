3e6ec7885d8d33835d797fde68db3aa4041b0145
ryanshim24 <ryanshim24@yahoo.com>
2014-11-18 11:42:04 -0800

    Fixed the modals autofocus removed flash messages because it looked weird

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
+
+  // MY Forget Password Modal
   $(".reset-link").on("click", function() {
     $('#logModal').modal('hide');
   });
 
+  $('.modal').on('shown.bs.modal', function() {
+    $(this).find('[autofocus]').focus();
+  });
+
+
+  // THE CHANGING OF MY NAV-BAR COLOR 
   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+
     if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
+      $(document).scroll(function() { 
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
           $(".navbar-default").css('background', 'rgba(255,255,255,0.85)');
           $('.navbar-default').css('border-bottom-color', 'rgba(0, 0, 0, 0.0980392)');
-
-       } else {
+        } else {
           $('.navbar-default').css('background-color', 'transparent');
           $('.navbar-default').css('border-bottom-color', 'rgba(0, 0, 0, 0)');
           $(".navbar-default").css('background', 'none ');
-       }
-   });
-    }
+        }
+      });
+    }//end of start changed
 
-});
+
+});//DOCUMENT CLOSED
+
+
+
+
